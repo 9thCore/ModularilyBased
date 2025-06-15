076f63ea526a4808d7065c5855794996f1b604eb
9thCore <38700349+9thCore@users.noreply.github.com>
2025-06-15 23:49:53 +0300

    basic identification component
    mostly for knowing if the patch was already applied for this face, because they get reinitialised when the base is changed

---
+++ Patch/BaseFaceIdentifier.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace ModularilyBased.Patch
+{
+    public class BaseFaceIdentifier : MonoBehaviour
+    {
+        public TechType room = TechType.None;
+        public FaceType face = FaceType.None;
+        public BoxCollider collider;
+
+        public enum FaceType
+        {
+            None,
+            AnySide,
+            LongSide,
+            ShortSide,
+            // Top,
+            // Bottom,
+            // Center
+        }
+    }
+}
