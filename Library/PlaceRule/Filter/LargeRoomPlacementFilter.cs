af249f6a84317ea3c05de049ef086fd6b1630f73
9thCore <38700349+9thCore@users.noreply.github.com>
2025-06-22 08:23:49 +0300

    First (and probably last) builtin filter
    Meant for large rooms, such that they should only be buildable if they fit

---
+++ Library/PlaceRule/Filter/LargeRoomPlacementFilter.cs
@@ -0,0 +1,20 @@
+﻿namespace ModularilyBased.Library.PlaceRule.Filter
+{
+    public class LargeRoomPlacementFilter : BasePlacementFilter
+    {
+        public static readonly LargeRoomPlacementFilter HalfWidth = new LargeRoomPlacementFilter(2);
+        public static readonly LargeRoomPlacementFilter FullWidth = new LargeRoomPlacementFilter(4);
+
+        public int moduleLength;
+
+        public LargeRoomPlacementFilter(int length)
+        {
+            moduleLength = length - 1;
+        }
+
+        public override bool CanBuildOn(ModuleSnapper snapper, BaseFaceIdentifier identifier)
+        {
+            return identifier.CenterFaceIndex >= moduleLength;
+        }
+    }
+}
