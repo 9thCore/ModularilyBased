2d54c598adb5a179470100e43b53f8b9090e9915
9thCore <38700349+9thCore@users.noreply.github.com>
2025-06-22 08:23:00 +0300

    Add filtered placement rule

---
+++ Library/PlaceRule/Filter/BasePlacementFilter.cs
@@ -0,0 +1,7 @@
+﻿namespace ModularilyBased.Library.PlaceRule.Filter
+{
+    public abstract class BasePlacementFilter
+    {
+        public abstract bool CanBuildOn(ModuleSnapper snapper, BaseFaceIdentifier identifier);
+    }
+}
---
+++ Library/PlaceRule/FilteredPlacementRule.cs
@@ -0,0 +1,61 @@
+﻿using ModularilyBased.Library.PlaceRule.Filter;
+using System.Collections.Generic;
+
+namespace ModularilyBased.Library.PlaceRule
+{
+    public class FilteredPlacementRule : PlacementRule
+    {
+        public readonly HashSet<BasePlacementFilter> filters = new();
+
+        public FilteredPlacementRule(SnapType snap) : base(snap)
+        {
+
+        }
+
+        public FilteredPlacementRule(SnapType snap, params BasePlacementFilter[] filters) : base(snap)
+        {
+            this.filters.AddRange(filters);
+        }
+
+        public FilteredPlacementRule(SnapType snap, IEnumerable<BasePlacementFilter> filters) : base(snap)
+        {
+            this.filters.AddRange(filters);
+        }
+
+        public FilteredPlacementRule AddFilter(BasePlacementFilter filter)
+        {
+            filters.Add(filter);
+            return this;
+        }
+
+        public FilteredPlacementRule AddFilters(IEnumerable<BasePlacementFilter> filters)
+        {
+            this.filters.AddRange(filters);
+            return this;
+        }
+
+        public FilteredPlacementRule AddFilters(params BasePlacementFilter[] filters)
+        {
+            this.filters.AddRange(filters);
+            return this;
+        }
+
+        public override bool CanBuildOn(ModuleSnapper snapper, BaseFaceIdentifier identifier)
+        {
+            if (!base.CanBuildOn(snapper, identifier))
+            {
+                return false;
+            }
+
+            foreach (BasePlacementFilter filter in filters)
+            {
+                if (!filter.CanBuildOn(snapper, identifier))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
