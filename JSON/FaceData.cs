e2436a1f8fd93070914ec409100df2e8cc6fcc9f
9thCore <38700349+9thCore@users.noreply.github.com>
2025-06-23 18:42:48 +0300

    Face data JSON storage and retrieval

---
+++ JSON/FaceData.cs
@@ -0,0 +1,17 @@
+﻿using ModularilyBased.Library;
+using UnityEngine;
+
+namespace ModularilyBased.JSON
+{
+    public class FaceData
+    {
+        public BaseFaceIdentifier.FaceType face = BaseFaceIdentifier.FaceType.None;
+        public Base.Face seabaseFace = default;
+        public int centerFaceIndex = 0;
+        public Vector3 position = Vector3.zero;
+        public Vector3 colliderPosition = Vector3.zero;
+        public Vector3 scale = Vector3.one;
+        public Quaternion rotation = Quaternion.identity;
+        public Quaternion colliderRotation = Quaternion.identity;
+    }
+}
---
+++ JSON/RoomFaceData.cs
@@ -0,0 +1,57 @@
+﻿using Nautilus.Extensions;
+using Nautilus.Json;
+using Nautilus.Utility;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace ModularilyBased.JSON
+{
+    public class RoomFaceData : JsonFile
+    {
+        [JsonIgnore]
+        public readonly string filename;
+
+        [JsonIgnore]
+        private string jsonFilePath;
+        public override string JsonFilePath => jsonFilePath ??= Path.Combine(DefinitionsPath, $"{filename}.json");
+
+        // Index by Base.CellType. Probably will require another identifier for custom base pieces, hence string instead of enum.
+        public Dictionary<string, List<FaceData>> storage = new();
+
+        public RoomFaceData(string filename)
+        {
+            this.filename = filename;
+        }
+
+        public static bool TryLoadRoomData(string identifier, out RoomFaceData data)
+        {
+            if (faceCache.TryGetValue(identifier, out data))
+            {
+                return true;
+            }
+
+            data = new RoomFaceData(identifier);
+
+            if (!Directory.Exists(Path.GetDirectoryName(data.JsonFilePath))
+                || !File.Exists(data.JsonFilePath))
+            {
+                data = null;
+                return false;
+            }
+
+            data.LoadWithConverters(false, new Int3Converter());
+            return true;
+        }
+
+        private static string assemblyPath;
+        public static string AssemblyPath => assemblyPath ??= Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        private static string definitionsPath;
+        public static string DefinitionsPath => definitionsPath ??= Path.Combine(AssemblyPath, "Definitions");
+
+        public static readonly Dictionary<string, RoomFaceData> faceCache = new();
+    }
+}
