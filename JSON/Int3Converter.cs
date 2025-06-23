9b1d47c029e6c7dfd80ae98dd90b1e57a8c7535e
9thCore <38700349+9thCore@users.noreply.github.com>
2025-06-23 18:38:37 +0300

    Add Int3 JSON converter

---
+++ JSON/Int3Converter.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace ModularilyBased.JSON
+{
+    public class Int3Converter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(Int3);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return (Int3)serializer.Deserialize<Int3Json>(reader);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            Int3 package = (Int3)value;
+            serializer.Serialize(writer, (Int3Json)package);
+        }
+    }
+
+    public record Int3Json(int X, int Y, int Z)
+    {
+        public static explicit operator Int3(Int3Json i) => new(i.X, i.Y, i.Z);
+        public static explicit operator Int3Json(Int3 i) => new(i.x, i.y, i.z);
+    }
+}
