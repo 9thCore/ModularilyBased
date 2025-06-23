e71490052957994fdd1c95bca233326cc29c36f2
9thCore <38700349+9thCore@users.noreply.github.com>
2025-06-23 18:42:25 +0300

    Create SnapHolder.cs

---
+++ Library/SnapHolder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace ModularilyBased.Library
+{
+    public class SnapHolder : MonoBehaviour
+    {
+        public GameObject root;
+
+        public void SetSibling(Transform sibling)
+        {
+            root = new GameObject();
+            root.transform.SetParent(sibling.parent);
+            root.transform.localPosition = sibling.localPosition;
+            root.transform.localRotation = sibling.localRotation;
+        }
+    }
+}
