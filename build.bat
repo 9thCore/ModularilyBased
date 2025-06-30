5c9969e951a4d63bb1fd72b354c0efed13749c68
9thCore <38700349+9thCore@users.noreply.github.com>
2025-06-30 10:38:30 +0300

    bump version and add xml doc to release
    lol

--- build.bat
+++ build.bat
@@ -2,5 +2,6 @@ set mod=ModularilyBased
 set modpath=%SUBNAUTICA_PATH%\BepInEx\plugins\%mod%
 mkdir "%modpath%"
 copy "bin\Release\net472\%mod%.dll" "%modpath%\%mod%.dll"
+copy "bin\Release\net472\%mod%.xml" "%modpath%\%mod%.xml"
 robocopy "Definitions" "%modpath%\Definitions" /E
 exit /B 0
